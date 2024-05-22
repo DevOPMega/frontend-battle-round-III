112162874667871a604a187d9252454a03f73e34
Adesh Singh <adeshsingh3002@gmail.com>
2024-05-22 11:58:23 +0530

    padding shift

--- app/messaging/page.js
+++ app/messaging/page.js
@@ -3,7 +3,11 @@ import Link from "next/link"
 export default function Messaging() {
     return (
         <>
-            
+            <div className="bg-zinc-950 border-slate-700 border  lg:w-1/2 lg:mx-auto lg:mt-6">
+                <h1 className="p-5 text-2xl text-center">
+                    Adesh Singh Message System
+                </h1>
+            </div>
         </>
     )
 }
--- app/user-profile/page.js
+++ app/user-profile/page.js
@@ -2,18 +2,9 @@ export default function UserProfile() {
     return (
         <>
             <div className="bg-zinc-950 border-slate-700 border  lg:w-1/2 lg:mx-auto lg:mt-6">
-                <div className="bg-slate-950">
-                    <div className="relative w-full h-40 bg-zinc-800">
-                        <div className="absolute -bottom-8 left-24">
-                            <div className="w-28 h-28 bg-white rounded-full">
-
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div className="mt-14 px-16 py-8">
-                    <h2>Adesh Singh</h2>
-                </div>
+                <h1 className="p-5 text-2xl text-center">
+                    Adesh Singh Profile Page
+                </h1>
             </div>
         </>
     )
